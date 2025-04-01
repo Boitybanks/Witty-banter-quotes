b9341e14ce7403b9cecb80863837d618816aaa75
Boity <mrsboitymbhele@gmail.com>
2025-04-01 17:24:08 +0200

    added events on buttons

--- src/index.js
+++ src/index.js
@@ -16,3 +16,22 @@ function generateWittyQuote(event) {
     cursor: " ",
   });
 }
+
+let getnewquote = document.querySelector("#new-quote");
+console.log(getnewquote);
+getnewquote.addEventListener("click", generateNewQuote);
+
+function generateNewQuote(event) {
+  event.preventDefault();
+
+  let getnewquote = document.querySelector("#new-quote");
+  getnewquote.innerHTML = "Generating a new qoute on the same topic.";
+  getnewquote.style.color = "black";
+
+  new Typewriter("#quote", {
+    strings: "Generating a new quote on the same topic.",
+    autoStart: true,
+    DelayNode: 1,
+    cursor: " ",
+  });
+}
